7b88256316a6ad3d00b7bf93c2afcbf8f8cb10dd
Daniel <daniel4wood@hotmail.com>
2018-05-24 19:02:27 -0700

    updated readme and commenting in baby.hs

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 This repo contains code used in the Haskell learning resource http://learnyouahaskell.com.
 
+baby.hs
+
+5 Functions that displays basic function syntax
+
+
 
--- baby.hs
+++ baby.hs
@@ -1,11 +1,24 @@
+-- Daniel Forwood
+--
+-- Functions from the book learnyouahaskell.
+-- v 1.0
+-- 24-05-2018
+
+
+-- Doubles a number
 doubleMe x= x+x
 
+-- Doubles 2 numbers and adds them together
 doubleUs x y = x*2 + y*2
 
+-- Doubles 2 numbers and adds them together
 doubleUs2 x y = doubleMe x + doubleMe y
 
+-- Doubles a number only if it is less than or equal to 100
 doubleSmallNumber x = if x > 100
   then x
   else x*2
 
+-- Doubles a number only if it is less than or equal to 100, then
+-- adds 1 regardless of the number.
 doubleSmallNumber' x = (if x > 100 then x else x*2)+1
